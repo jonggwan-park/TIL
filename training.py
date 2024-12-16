af124a86d290e34904d2891b75384b5eb05cc8b4
jonggwan-park <ds3hfj2@gmail.com>
2024-12-16 22:31:59 +0900

    til

--- training.py
+++ training.py
@@ -1,24 +1,10 @@
-class Student:
-    '''
-    후.....
-    하기 싫다..
-    '''
-    scholarship_score = 80
+import pymc3 as pm
+import numpy as np
 
+with pm.Model() as model:
+    mu = pm.Normal('mu', mu=0, sd=1)
+    obs = pm.Normal('obs', mu=mu, sd=1, observed=np.random.randn(100))
+    trace = pm.sample(1000)
 
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
-
-
-    def get_grade(self):
-        print(f'called >>>> get_grade')
-        return f'return >>>> {self.grade}'
-    
-    def scholarship_up(self):
-        Student.scholarship_score +=5 
-
-a=Student('aiden',13,6)
-a.scholarship_up()
-print(a.scholarship_score)
+pm.plot_posterior(trace)
+plt.show()
